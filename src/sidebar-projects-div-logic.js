662ed661fdc5e89af99a3e3eede3a8fcb4197548
fredieposh <maor1234@gmail.com>
2025-08-21 18:30:43 +0300

    create tasks logic

--- src/sidebar-projects-div-logic.js
+++ src/sidebar-projects-div-logic.js
@@ -1,9 +1,11 @@
+import {createProjectTasksList} from "./tasks-logic.js"
 export {projectList, addProject, removeProject};
 
 const projectList =[];
 
 function addProject(projectName) {
     projectList.push(projectName);
+    createProjectTasksList(projectName);
 }
 
 function removeProject(projectName) {
---
+++ src/tasks-logic.js
@@ -0,0 +1,63 @@
+export {createProjectTasksList, Task};
+
+const projectsTasks = {};
+
+function createProjectTasksList(projectName) {
+    projectsTasks[projectName] = [];
+}
+
+class Task {
+    #projectName;
+    #taskId;
+    #taskTitle;
+    #taskDesc;
+    #taskDueDate;
+    #taskPrior;
+
+    constructor(projectName, taskTitle) {
+        this.#projectName = projectName;
+        this.#taskId =  projectsTasks[projectName].length;
+        this.#taskTitle = taskTitle;
+    };
+
+    getTaskProjectName(){
+        return this.#projectName;
+    };
+
+    getTaskId() {
+        return this.#taskId;
+    };
+
+    setTaskTitle(taskTitle) {
+        this.#taskTitle = taskTitle;
+    };
+
+    getTaskTitle() {
+        return this.#taskTitle;
+    };
+
+    setTaskDesc(TaskDesc) {
+        this.#taskDesc = TaskDesc;
+    };
+
+    getTaskDesc() {
+        return this.#taskDesc;
+    };
+
+    setTaskDueDate(TaskDueDate) {
+        this.#taskDueDate = TaskDueDate;
+    };
+
+    getTaskDueDate() {
+        return this.#taskDueDate;
+    };
+
+    setTaskPrior(TaskPrior) {
+        this.#taskPrior = TaskPrior;
+    };
+
+    getTaskPrior() {
+        return this.#taskPrior;
+    };
+
+}
